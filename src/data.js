dc4abd1dafa1abf7e8f0178b3ba4b4ca4f0bb7dc
ahlem-20 <ahlemmh123@gmail.com>
2022-10-19 11:37:41 +0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,10 @@
+export const inventoryData = [
+  { name: 'Hat', price: 23.99, img: 'https://static-assets.codecademy.com/Courses/Learn-Redux/codecademy-store/hello-world-hat.jpg' },
+  { name: 'Joggers', price: 45.99, img: 'https://static-assets.codecademy.com/Courses/Learn-Redux/codecademy-store/sweatpants.jpg' },
+  { name: 'T shirt white', price: 17.99, img: 'https://static-assets.codecademy.com/Courses/Learn-Redux/codecademy-store/java-tee.jpg' },
+  { name: 'T shirt black', price: 17.99, img: 'https://static-assets.codecademy.com/Courses/Learn-Redux/codecademy-store/python-tee.jpg' },
+  { name: 'T shirt black Men', price: 17.99, img: 'https://static-assets.codecademy.com/Courses/Learn-Redux/codecademy-store/sql-tee.jpg' },
+  { name: 'Hoodie', price: 49.99, img: 'https://static-assets.codecademy.com/Courses/Learn-Redux/codecademy-store/hoodie.jpg' },
+];
+
+export const currenciesData = ['USD', 'EUR', 'CAD'];
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { App } from './App.js';
+import { store } from './store';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const render = () => {
 root.render(
   <React.StrictMode>
-    <App />
+    <App 
+      state={store.getState()}
+      dispatch={store.dispatch}
+
+       />
   </React.StrictMode>
 );
-
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+render();
+store.subscribe(render);
